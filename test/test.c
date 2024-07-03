c7dff4a82a9c9411e9be02590e346eaa5290d8f4
qgymib@gmail.com <4520263+qgymib@users.noreply.github.com>
2024-07-03 11:26:51 +0800

    Update CMakeLists.txt, main.c, test.c, and 1 more file

--- test/test.c
+++ test/test.c
@@ -1,3 +1,62 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 #include "test.h"
+#include "tools/__init__.h"
 
 popen3_test_ctx_t _G;
+
+static char* _popen3_test_exepath(void)
+{
+    size_t buff_sz = 4096;
+    char* buff = malloc(buff_sz);
+    if (readlink("/proc/self/exe", buff, buff_sz) < 0)
+    {
+        abort();
+    }
+
+    return buff;
+}
+
+static void _before_all_test(int argc, char* argv[])
+{
+    int i;
+    for (i = 0; i < argc; i++)
+    {
+        if (strcmp(argv[i], "--") == 0)
+        {
+            i++;
+
+            if (i == argc)
+            {
+                fprintf(stderr, "missing tool name\n");
+                exit(EXIT_FAILURE);
+            }
+
+            int ret = popen3_test_tool_exec(argv[i], argc - i, argv + i);
+            exit(ret);
+        }
+    }
+
+    _G.SELF_PATH = _popen3_test_exepath();
+}
+
+static void _after_all_test(void)
+{
+    if (_G.SELF_PATH != NULL)
+    {
+        free(_G.SELF_PATH);
+        _G.SELF_PATH = NULL;
+    }
+}
+
+cutest_hook_t g_hook = {
+    _before_all_test, /* .before_all_test */
+    _after_all_test,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+};
--- test/test.h
+++ test/test.h
@@ -1,6 +1,8 @@
 #ifndef POPEN3_TEST_H
 #define POPEN3_TEST_H
 
+#include "cutest.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -10,13 +12,18 @@ typedef struct popen3_test_ctx
     /**
      * @brief Path to self.
      */
-    const char* SELF_PATH;
+    char* SELF_PATH;
 } popen3_test_ctx_t;
 
 /**
  * @brief Global test runtime.
  */
 extern popen3_test_ctx_t _G;
+
+/**
+ * @brief Test hook.
+ */
+extern cutest_hook_t g_hook;
 
 #ifdef __cplusplus
 }
